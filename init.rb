82271d803c8e1b5735b77b2d6197150d10a99d6d
geemus <geemus@gmail.com>
2012-06-19 16:42:09 -0500

    add redefinition of db methods, modify to use vendored taps

--- init.rb
+++ init.rb
@@ -1,3 +1,6 @@
 Dir[File.join(File.expand_path("../vendor", __FILE__), "*")].each do |vendor|
   $:.unshift File.join(vendor, "lib")
 end
+
+require("heroku/command/db")
+require("#{File.dirname(__FILE__)}/lib/ext/heroku/command/db")
